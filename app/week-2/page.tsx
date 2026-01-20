5a7e619b0f28eddee576d9e6eda8e61af71ebafc
Tess <70186751+zinc-saucier@users.noreply.github.com>
2026-01-20 14:34:05 -0700

    new assignment

---
+++ app/week-2/page.tsx
@@ -0,0 +1,11 @@
+import StudentInfo from "./studentInfo";
+
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo/>
+    </main>
+  );
+}
---
+++ app/week-2/studentInfo.tsx
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Name: Tessa Unrau</h1>
+      <h2>Github: https://github.com/zinc-saucier/cprg306-assignments</h2>
+    </main>
+  );
+}
